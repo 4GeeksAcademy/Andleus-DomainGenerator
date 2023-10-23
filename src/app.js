9c1e260dd98da8f9c19b4552c0b75b650d1add7f
Andleus <abadiaandres@gmail.com>
2023-10-23 19:27:30 +0200

    Project done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let showConsole = document.querySelector("#showConsole");
+
+  let result = "";
+  let resultForConsole = "";
+  let cookingResult = "";
+
+  let arra1 = ["the", "our"];
+  let arra2 = ["great", "big"];
+  let arra3 = ["jogger", "racoon"];
+  let arra4 = [".com", ".net", "us", ".io", "es"];
+
+  cookingResult = arra1.flatMap(a1 =>
+    arra2.flatMap(a2 => arra3.flatMap(a3 => arra4.map(a4 => a1 + a2 + a3 + a4)))
+  );
+
+  for (let i of cookingResult) {
+    result += `${i}<br>`;
+  }
+
+  for (let i of cookingResult) {
+    resultForConsole += `${i}\n`;
+  }
+
+  showConsole.innerHTML = result;
+  console.log(resultForConsole);
 };
